f8e3b54510a97e89fe3bf4bbb0b2c54aaa9b599a
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-08-12 17:16:13 +0530

    tags

--- src/app/home/video-section.js
+++ src/app/home/video-section.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
