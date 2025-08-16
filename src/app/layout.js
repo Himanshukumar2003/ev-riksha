070a49397de474e4806181177de42b1b22759a24
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-08-16 12:41:37 +0530

    hide page

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,7 @@ import { Manrope } from "next/font/google";
 import Layout from "@/components/layout/layout";
 import { Toaster } from "sonner";
 import Script from "next/script";
+import Image from "next/image";
 
 // Local SpeedBeast font
 const SpeedBeast = localFont({
@@ -159,6 +160,29 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '932744845716336');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <Image
+            height={64}
+            width={64}
+            style={{ display: "none" }}
+            alt="Facebook Pixel"
+            src="https://www.facebook.com/tr?id=932744845716336&ev=PageView&noscript=1"
+          />
+        </noscript>
       </head>
 
       <meta
@@ -166,6 +190,7 @@ export default function RootLayout({ children }) {
         content="eMK-J7AIIge48keTYluNj9JqmmIGzGLqgoohVld9lB4"
       />
       <GoogleTagManager gtmId="G-5Z8REY1QK8" />
+
       <body
         className={`${SpeedBeast.variable} ${manrope.variable} antialiased`}
         suppressHydrationWarning
