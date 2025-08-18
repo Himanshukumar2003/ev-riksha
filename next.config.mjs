955404e2675c17b5eab8104312c32b01d99b86ce
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-08-18 11:38:04 +0530

    api

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["img.youtube.com", "macapi.brandingwaale.com"],
+    domains: ["img.youtube.com", "api.macautoindia.com"],
   },
 };
 
