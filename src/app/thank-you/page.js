c9be0b74e37fd61b579a44c57adbffdccdf95b9e
Himanshu Kumar <devliyalhimanshu@gmail.com>
2025-08-29 13:52:43 +0530

    res

--- src/app/thank-you/page.js
+++ src/app/thank-you/page.js
@@ -1,5 +1,3 @@
-// import ThankYou from "./_component/thank-you";
-
-// export default function ThankYouPage() {
-//   return <ThankYou></ThankYou>;
-// }
+export default function ThankYouPage() {
+  return <h1>hhh</h1>;
+}
