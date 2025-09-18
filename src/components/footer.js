eee2d7e0cb04e6eda53c8b6fdd1f26f1edb93df1
unknown <devliyalhimanshu@gmsil.com>
2025-09-18 11:03:35 +0530

    add pages

--- src/components/footer.js
+++ src/components/footer.js
@@ -30,6 +30,7 @@ export default function Footer() {
     { title: "Kanpur Dehat", city: "kanpur-dehat" },
     { title: "Lucknow", city: "lucknow" },
     { title: "Prayagraj", city: "prayagraj" },
+    
   ];
   return (
     <>
